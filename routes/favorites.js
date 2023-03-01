1060a7c3a26cd2375f6c5639ef5e2d1a7784bfc2
loloexotic <loloexotic@airdeloloexotic.lan>
2023-03-01 16:25:29 +0100

    change

--- routes/favorites.js
+++ routes/favorites.js
@@ -3,7 +3,7 @@ const { findById } = require("../models/Card");
 const Card = require("../models/Card");
 const router = express.Router();
 
-router.get("/favourites", async (res, req) => {
+router.get("/favorites", async (res, req) => {
   // const favorite = await Card.findById(req.body.id)
   //  trouver et stocker l'id/le favoris dans la BDD
   // methode à utiliser ?
